9443001363995f8e6a0df23536a13359750a9512
zheer barzan <zheerbarzan85@outlook.com>
2024-01-25 22:28:43 +0300

    other data types

--- syntax/syntax.swift
+++ syntax/syntax.swift
@@ -5,8 +5,32 @@ var name = "zheer"
 let pi = 3.14
 
 // to print a value
-
 print(name,pi)
+
+// making an array
+var colors = ["red", "green", "blue"]
+print(colors)
+
+
+// to make a dictionary
+let employe = [
+    "name" : "tyalor",
+    "job" : "singer"
+]
+print(employe["job",default: "unknown"])
+
+// to make a set (cannot add duplicate item and they have no order)
+var numbers = Set([1,1,2,3,4,5])
+print(numbers)
+
+// to make enums
+enum weekday{
+    case monday,tuesday,wednesday, thursday, friday
+}
+
+var day = weekday.friday
+print(day)
+
 
 
 
